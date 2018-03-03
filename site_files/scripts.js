d46623792f04cd38a5677b41fc062da2f4b07e31
Hunter Jefferson <hunterjefferson@Hunters-MBP.austin.rr.com>
2018-03-03 16:56:08 -0600

    day 2

---
+++ site_files/scripts.js
@@ -0,0 +1,42 @@
+$(function () {
+  const config = {
+    apiKey: "AIzaSyBEuz8CjkppEfcz-i1el4YgzS6VudEdqnY",
+    authDomain: "q1-project-77b24.firebaseapp.com",
+    databaseURL: "https://q1-project-77b24.firebaseio.com",
+    projectId: "q1-project-77b24",
+    storageBucket: "q1-project-77b24.appspot.com",
+    messagingSenderId: "323670236834"
+  };
+  firebase.initializeApp(config);
+
+  // const state = {
+  //   email: "",
+  //   password: "",
+  // }
+
+  $('.js-form').on('submit', event => {
+    event.preventDefault();
+    // const email = $('#js-email').val() || state.email;
+    const email = $('#js-email').val();
+    // const password = $('#js-password').val() || state.password;
+    const password = $('#js-password').val();
+    const imageID = $('#js-image-id').val();
+    const imageDate = $('#js-image-date').val();
+    const imageKeywords = $('#js-image-keywords').val().split(" ");
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+          .then(user => {
+            // state.email = email;
+            // state.password = password;
+            // $('#js-login-data').addClass('hidden');
+            firebase.database().ref('images').push({
+                imageID, imageDate, imageKeywords
+              });
+              console.log(imageID, imageDate, imageKeywords);
+              $('.js-form').trigger('reset');
+            })
+          .catch(error => {
+            console.log(error);
+          });
+  });
+});
