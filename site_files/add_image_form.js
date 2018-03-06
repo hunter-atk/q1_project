a9a0b07a4e3e4c334020c560321a045ab47d3cc1
Hunter Jefferson <hunterjefferson@Hunters-MBP.austin.rr.com>
2018-03-05 20:15:35 -0600

    archive inputs working. Now must enable specifications and search

---
+++ site_files/add_image_form.js
@@ -0,0 +1,56 @@
+const config = {
+  apiKey: "AIzaSyBEuz8CjkppEfcz-i1el4YgzS6VudEdqnY",
+  authDomain: "q1-project-77b24.firebaseapp.com",
+  databaseURL: "https://q1-project-77b24.firebaseio.com",
+  projectId: "q1-project-77b24",
+  storageBucket: "q1-project-77b24.appspot.com",
+  messagingSenderId: "323670236834"
+};
+firebase.initializeApp(config);
+
+$(function () {
+  const state = {
+    email: "",
+    password: "",
+  }
+    var fileName;
+
+    //Image Upload
+    var imageUpload = document.getElementById('js-image-upload');
+    imageUpload.addEventListener('change', function(e){
+      var file = e.target.files[0];
+      fileName = file.name;
+      var storageRef = firebase.storage().ref('images/' + file.name);
+      storageRef.put(file);
+    })
+
+  $('.js-form').on('submit', event => {
+    event.preventDefault();
+
+    const email = $('#js-email').val() || state.email;
+    //const email = $('#js-email').val();
+    const password = $('#js-password').val() || state.password;
+    //const password = $('#js-password').val();
+    const imageID = $('#js-image-id').val();
+    const imageDate = $('#js-image-date').val();
+    const imageKeywords = $('#js-image-keywords').val().split(" ");
+
+
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+          .then(user => {
+            state.email = email;
+            state.password = password;
+            $('#js-login-data').addClass('hidden');
+            //$('#js-email').addClass('hidden');
+            firebase.database().ref('images').push({
+                imageID, imageDate, imageKeywords, fileName
+              });
+              console.log(imageID, imageDate, imageKeywords);
+              $('.js-form').trigger('reset');
+            })
+          .catch(error => {
+            console.log(error);
+          });
+  });
+});
--- site_files/scripts.js
+++ site_files/scripts.js
@@ -8,54 +8,63 @@ const config = {
 };
 firebase.initializeApp(config);
 
-$(function () {
-  const state = {
-    email: "",
-    password: "",
-  }
-    var fileName;
+//reference image storage and doc elements
+var ref = firebase.database().ref('images');
+ref.on('value', gotData, errData);
+var archiveResultsContainer = document.getElementById('archiveResultsContainer');
 
-    //Image Upload
-    var imageUpload = document.getElementById('js-image-upload');
-    imageUpload.addEventListener('change', function(e){
-      var file = e.target.files[0];
-      fileName = file.name;
-      var storageRef = firebase.storage().ref('images/' + file.name);
-      storageRef.put(file);
-    })
+//helper function to append image metadata to div
+function appendMetadata(imageId, imageDate, newImageResult, newImageMetadata){
+  var imgIdDiv = document.createElement("div");
+  imgIdDiv.innerText = imageId;
+  var imgDateDiv = document.createElement("div");
+  imgDateDiv.innerText = imageDate;
+  newImageMetadata.appendChild(imgIdDiv);
+  newImageMetadata.appendChild(imgDateDiv);
+}
 
-  $('.js-form').on('submit', event => {
-    event.preventDefault();
+//helper function to add new img and tag html to archiveResultsContainer
+function addImageResult(storedImage, imageId, imageDate){
+  var newResult = document.createElement("div");
+  newResult.setAttribute("class", "newResultContainer");
+  archiveResultsContainer.appendChild(newResult);
+  var newImageResult = document.createElement("img");
+  var newImageMetadata = document.createElement("div");
+  newImageResult.setAttribute("src", storedImage);
+  newImageResult.setAttribute("class", "imgThumbnail");
+  newImageMetadata.setAttribute("class", "imgMetadata");
+  newResult.appendChild(newImageResult);
+  newResult.appendChild(newImageMetadata);
+  appendMetadata(imageId, imageDate, newImageResult, newImageMetadata);
+}
 
-    const email = $('#js-email').val() || state.email;
-    //const email = $('#js-email').val();
-    const password = $('#js-password').val() || state.password;
-    //const password = $('#js-password').val();
-    const imageID = $('#js-image-id').val();
-    const imageDate = $('#js-image-date').val();
-    const imageKeywords = $('#js-image-keywords').val().split(" ");
 
+//Retrieve data if data exists
+let imageCalls = [];
+function gotData(data){
+  var images = data.val();
+  var keys = Object.keys(images);
+  for (let i=0; i<keys.length; i++){
+    var k = keys[i];
+    var imageDisplay = images[k].fileName;
+    var imageId = images[k].imageID;
+    var imageDate = images[k].imageDate;
 
+    getImage(imageId,imageDisplay,imageDate)
+    function getImage(id, display, date){
+      firebase.storage().ref().child('images/' + display).getDownloadURL()
+      .then( (url) =>{
+        addImageResult(url, id, date)
+      })
+      .catch(function(error) {
+      });
+    }
+   }
 
-    firebase.auth().signInWithEmailAndPassword(email, password)
-          .then(user => {
-            state.email = email;
-            state.password = password;
-            $('#js-login-data').addClass('hidden');
-            //$('#js-email').addClass('hidden');
-            firebase.database().ref('images').push({
-                imageID, imageDate, imageKeywords, fileName
-              });
-              console.log(imageID, imageDate, imageKeywords);
-              $('.js-form').trigger('reset');
-            })
-          .catch(error => {
-            console.log(error);
-          });
-  });
-});
+ }
 
-function getData(){
-  var resultsContainer = document.getElementById('archiveResultsContainer');
-  var result
+//Return error if no data exists
+function errData(err){
+  console.log('Error!');
+  console.log(err);
 }
